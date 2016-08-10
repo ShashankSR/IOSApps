29c633d901ec6bcdf67c72b8887c04efa1e73590
ShashankSR <shashanksheshan@gmail.com>
2016-08-10 14:31:51 +0530

    Fix UI thread update error

--- ShashankSR/Assign2ViewController.swift
+++ ShashankSR/Assign2ViewController.swift
@@ -19,6 +19,8 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var proglabel: UILabel!
+    @IBOutlet weak var label: UILabel!
     
     lazy var downloadsSession: NSURLSession = {
         let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
@@ -30,37 +32,40 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
         super.viewDidLoad()
         progressBar.progress = 0
         progressBar.hidden = true
+        proglabel.hidden = true
+        label.hidden = true
         imageView.hidden = true
         // Do any additional setup after loading the view.
     }
 
     @IBAction func handleSearchGo(sender: AnyObject) {
         
-        progressBar.hidden = false
+      
         if !searchBar.text!.isEmpty {
-            // 1
+            
             if dataTask != nil {
                 dataTask?.cancel()
             }
-            // 2
+          
             UIApplication.sharedApplication().networkActivityIndicatorVisible = true
-            // 4
+          
             let url = NSURL(string: searchBar.text!)
-            // 5
+          
             dataTask = defaultSession.dataTaskWithURL(url!) {
                 data, response, error in
-                // 6
+                
                 dispatch_async(dispatch_get_main_queue()) {
                     UIApplication.sharedApplication().networkActivityIndicatorVisible = false
                 }
-                // 7
+               
                 if let error = error {
                     print(error.localizedDescription)
                 } else if let httpResponse = response as? NSHTTPURLResponse {
                     if httpResponse.statusCode == 200 {
-                        if(httpResponse.MIMEType! == "image/jpeg"){
+                        if(httpResponse.MIMEType!.lowercaseString.rangeOfString("image") != nil){
                             self.isImage = true
                         }
+                        print(httpResponse)
                         self.startDownload((httpResponse.URL?.absoluteString)!)
                     }
                 }
@@ -75,10 +80,7 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
         // Dispose of any resources that can be recreated.
     }
     
-   /* func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
-            print("Finished downloading.")
-    }
-    */
+   
    
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
         // 1
@@ -87,9 +89,10 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
             let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0])
         
             let destinationURL = documentsPath.URLByAppendingPathComponent(originalURL!)
-           // print(destinationURL)
         
+            // http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4
             //https://img.pokemondb.net/artwork/charizard.jpg
+            //http://wallpapercave.com/wp/Qf9SNmS.png
             // 2
         
             let fileManager = NSFileManager.defaultManager()
@@ -106,24 +109,23 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
                     print("Setting img")
                     let imgdata = NSData(contentsOfURL:(downloadTask.originalRequest?.URL)!)
                     dispatch_async(dispatch_get_main_queue(), {
-                       self.imageView.image = UIImage(data:imgdata!)
+                        self.imageView.image = UIImage(data:imgdata!)
                     })
                     progressBar.hidden = true
+                    proglabel.hidden = true
+                    label.hidden = true
                     imageView.hidden = false
                 }else{
                     let alertController = UIAlertController(title: "Complete", message: "Download Complete", preferredStyle: .Alert)
                     
                     let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
                     alertController.addAction(defaultAction)
-                    
-                    presentViewController(alertController, animated: true, completion: nil)
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.presentViewController(alertController, animated: true, completion: nil)
+                    })
                 }
                 
               
-                
-               //
-               // let vc = UIActivityViewController(activityItems: fileManager, applicationActivities: )
-               // self.presentViewController(vc, animated: true, completion:nil )
                 let thedate = NSDate()
                 let datecomp = NSDateComponents()
                 datecomp.second = 1
@@ -136,6 +138,7 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
                 notification.fireDate = mfiredate
                 
                 UIApplication.sharedApplication().scheduleLocalNotification(notification)
+                
 
             } catch let error as NSError {
                 print("Could not copy file to disk: \(error.localizedDescription)")
@@ -144,40 +147,33 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
  
    
     func startDownload(str: String) {
-    
             let urlString = str
             let url =  NSURL(string: urlString)
-            // 1
             let download = Download(url: urlString)
-            // 2
             download.downloadTask = downloadsSession.downloadTaskWithURL(url!)
-            // 3
             download.downloadTask!.resume()
-            // 4
             download.isDownloading = true
-            // 5
-        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    // 1
 
 
     func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        // 1
-        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
         
-        dispatch_async(dispatch_get_main_queue()) {
-            self.progressBar.progress = progress
+        let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        if totalBytesExpectedToWrite < 0 {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.progressBar.hidden = true
+                self.proglabel.hidden = false
+                self.label.hidden = false
+                self.proglabel.text = NSString(format: "%.2f", Float(totalBytesWritten)/1024) as String
+            }
+           
+        } else {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.progressBar.hidden = true
+                self.proglabel.hidden = false
+                self.label.hidden = false
+                self.progressBar.progress = progress
+            }
         }
         
     }
@@ -187,7 +183,6 @@ class Assign2ViewController: UIViewController, NSURLSessionDelegate,NSURLSession
         var url: String
         var isDownloading = false
         var progress: Float = 0.0
-        
         var downloadTask: NSURLSessionDownloadTask?
         var resumeData: NSData?
         
